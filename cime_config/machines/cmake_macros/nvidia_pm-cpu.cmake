66d80332bca37ff269dce6a83f8f906bf1ce3007
noel <ndkeen@lbl.gov>
2022-05-16 10:58:50 -0700

    AH MAM5 perlmutter cherry pick #6

--- cime_config/machines/cmake_macros/nvidia_pm-cpu.cmake
+++ cime_config/machines/cmake_macros/nvidia_pm-cpu.cmake
@@ -20,7 +20,8 @@ if (NOT DEBUG)
   string(APPEND FFLAGS " -O2")
 endif()
 if (compile_threaded)
-  string(APPEND KOKKOS_OPTIONS " -DKokkos_ENABLE_OPENMP=Off") # work-around for nvidia as kokkos is not passing "-mp" for threaded build
+  #set(KOKKOS_OPTIONS "--with-serial --with-openmp")
+  set(KOKKOS_OPTIONS "--with-serial") # work-around for nvidia as kokkos is not passing "-mp" for threaded build
 endif()
 set(MPICC "cc")
 set(MPICXX "CC")
